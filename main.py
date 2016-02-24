6b73c8b2076494a5a6735899f665888fe55069af
nisiyu <nisiyu91@gmail.com>
2016-02-24 22:25:29 +0800

    调整main.py main函数，输入来源改为ConfigParser

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import Builder
+
+import ConfigParser
 def main(args):
-    try:
-        s = Builder.settings()
-        s.test_data()
-        s.calc_all_items_height()
-        s.align_items()
-        s.draw_all()
-    except Exception as ex:
-        print ex
+    s = ConfigParser.Parse()
+    s.calc_all_items_height()
+    s.align_items()
+    s.draw_all()
+
 
 main(1)
